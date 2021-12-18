5d8cf3286790c08057a94e25522755f4538f1f48
Charles <c_onyemem@live.com>
2021-12-17 20:58:26 -0800

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.addEventListener("submit", function(e){
+    e.preventDefault()
+   
+    newT(e.target.children[1].value)
+
+  })
+function newT(todos){
+  let list = document.createElement('li')
+  let btn = document.createElement('button')
+  
+  btn.addEventListener('click', removeTask)
+  
+  btn.textContent = 'X'
+  list.textContent = `${todos} `
+  list.append(btn)
+  
+  document.querySelector('#list').appendChild(list)
+  
+}
+
+function removeTask(e){
+e.target.parentNode.remove()
+}
+  
 });
